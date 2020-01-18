775f39a2321c53e3e6616423993784acbc62fc31
jideguru <guruliciousjide@gmail.com>
2020-01-18 03:48:21 +0100

    Used epub_view plugin for loading ePub files

--- lib/screen/downloads.dart
+++ lib/screen/downloads.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ebook_app/database/download_helper.dart';
+import 'package:flutter_ebook_app/screen/reader.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Downloads extends StatefulWidget {
   @override
@@ -8,6 +10,8 @@ class Downloads extends StatefulWidget {
 }
 
 class _DownloadsState extends State<Downloads> {
+//  static const pageChannel = const EventChannel('com.xiaofwang.epub_kitty/page');
+
   bool done = true;
   var db = DownloadsDB();
 
@@ -18,7 +22,6 @@ class _DownloadsState extends State<Downloads> {
       dls.addAll(l);
     });
   }
-
 
   @override
   void initState() {
@@ -40,133 +43,154 @@ class _DownloadsState extends State<Downloads> {
         itemBuilder: (BuildContext context, int index) {
           Map dl = dls[index];
 
-          return Padding(
-            padding: EdgeInsets.symmetric(vertical: 5),
-            child: Row(
-              children: <Widget>[
-                CachedNetworkImage(
-                  imageUrl: dl['image'],
-                  placeholder: (context, url) => Container(
-                    height: 70,
-                    width: 70,
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
+          return InkWell(
+            onTap: (){
+              Navigator.pushReplacement(
+                context,
+                PageTransition(
+                  type: PageTransitionType.rightToLeft,
+                  child: Reader(
+                    path: dl['path'],
                   ),
-                  errorWidget: (context, url, error) => Image.asset(
-                    "assets/images/place.png",
+                ),
+              );
+//              String iosBookPath = dl['path'];
+//              String androidBookPath = dl['path'];
+//              EpubKitty.setConfig("iosBook", "#32a852","vertical",true);
+//              EpubKitty.open(androidBookPath);
+//
+//              pageChannel.receiveBroadcastStream().listen((Object event) {
+//                print('page:$event');
+//              }, onError: null);
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                children: <Widget>[
+                  CachedNetworkImage(
+                    imageUrl: dl['image'],
+                    placeholder: (context, url) => Container(
+                      height: 70,
+                      width: 70,
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Image.asset(
+                      "assets/images/place.png",
+                      fit: BoxFit.cover,
+                      height: 70,
+                      width: 70,
+                    ),
                     fit: BoxFit.cover,
                     height: 70,
                     width: 70,
                   ),
-                  fit: BoxFit.cover,
-                  height: 70,
-                  width: 70,
-                ),
 
-                SizedBox(
-                  width: 10,
-                ),
-
-                Flexible(
-                  child: done
-                      ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        dl['name'],
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Divider(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "COMPLETED",
-                            style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).accentColor
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-
-                          Text(
-                            dl['size'],
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ],
-                  )
-                      : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Alice's Adventures in Wonderland",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(
-                       height: 5,
-                      ),
-                      Container(
-                        height: 5,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                        ),
-                        child: LinearProgressIndicator(
-                          value: 0.7,
-                          valueColor: AlwaysStoppedAnimation(Theme.of(context).accentColor),
-                          backgroundColor: Theme.of(context).accentColor.withOpacity(0.3),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "70%",
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-
-                          Text(
-                            "300kb of 415kb",
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-
-                    ],
+                  SizedBox(
+                    width: 10,
                   ),
-                ),
-              ],
+
+                  Flexible(
+                    child: done
+                        ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          dl['name'],
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "COMPLETED",
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).accentColor
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+
+                            Text(
+                              dl['size'],
+                              style: TextStyle(
+                                fontSize: 13,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                        : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Alice's Adventures in Wonderland",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                         height: 5,
+                        ),
+                        Container(
+                          height: 5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: LinearProgressIndicator(
+                            value: 0.7,
+                            valueColor: AlwaysStoppedAnimation(Theme.of(context).accentColor),
+                            backgroundColor: Theme.of(context).accentColor.withOpacity(0.3),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "70%",
+                              style: TextStyle(
+                                fontSize: 13,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+
+                            Text(
+                              "300kb of 415kb",
+                              style: TextStyle(
+                                fontSize: 13,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
---
+++ lib/screen/reader.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:epub_view/epub_view.dart';
+import 'package:flutter/material.dart';
+
+class Reader extends StatelessWidget {
+  final String path;
+
+  Reader({
+    Key key,
+    @required this.path
+  }):super(key: key);
+
+  Future<Uint8List> _loadFromStorage() async {
+    print(path);
+    return await File(path).readAsBytes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<EpubBook>(
+        future: _loadFromStorage().then(EpubReader.readBook),
+        builder: (_, snapshot) {
+          if (snapshot.hasData) {
+            return EpubReaderView(
+              book: snapshot.data,
+              // Called when scrolled to another chapter
+              headerBuilder: (value) => AppBar(
+                title: Text(
+                  'Chapter ${value?.chapter?.Title ?? 'Loading...'}',
+                ),
+              ),
+              // Start from special chapter
+//              startFrom: EpubReaderLastPosition(3),
+            );
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
