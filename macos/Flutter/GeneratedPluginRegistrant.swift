5ae0ba016fa4b0eb2edfd2cc4b17800d700d7e1f
jideguru <guruliciousjide@gmail.com>
2023-04-16 00:14:22 +0100

    chore: upgrade dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus_macos
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
