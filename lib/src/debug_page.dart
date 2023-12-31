6e83a654fff35e58fd734a8b6a2bed75ae2a05b9
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-01-01 00:09:36 +0530

    Update debug_page.dart
    Updated to Null Safety.
    Changed ref.read to context.read for accessing notifier instances.

--- lib/src/debug_page.dart
+++ lib/src/debug_page.dart
@@ -3,33 +3,33 @@ import 'package:flutter_ebook_app/src/common/common.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DebugPage extends ConsumerWidget {
-  const DebugPage({super.key});
+  const DebugPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(currentAppThemeNotifierProvider).value ==
-        CurrentAppTheme.dark;
+    final isDarkMode = context.read(currentAppThemeNotifierProvider).value == CurrentAppTheme.dark;
+
     return Scaffold(
       body: ListView(
         children: <Widget>[
           ListTile(
             title: const Text('Clear Downloads'),
             onTap: () {
-              ref.read(downloadsNotifierProvider.notifier).clearBooks();
+              context.read(downloadsNotifierProvider.notifier).clearBooks();
             },
           ),
           ListTile(
             title: const Text('Clear Favorites'),
             onTap: () {
-              ref.read(favoritesNotifierProvider.notifier).clearBooks();
+              context.read(favoritesNotifierProvider.notifier).clearBooks();
             },
           ),
           ListTile(
             title: Text('Change Theme to ${isDarkMode ? 'Light' : 'Dark'}'),
             onTap: () {
-              ref
-                  .read(currentAppThemeNotifierProvider.notifier)
-                  .updateCurrentAppTheme(!isDarkMode);
+              ref.read(currentAppThemeNotifierProvider.notifier).updateCurrentAppTheme(
+                isDarkMode ? CurrentAppTheme.light : CurrentAppTheme.dark,
+              );
             },
           ),
         ],
