25d69a032fea887c72010e139eee1a22dafa2e41
jideguru <guruliciousjide@gmail.com>
2024-04-20 17:19:47 +0100

    feat: usePathUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ebook_app/src/app.dart';
 import 'package:flutter_ebook_app/src/common/common.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:sembast/sembast.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   LocalStorage();
   await DatabaseConfig.init(StoreRef<dynamic, dynamic>.main());
+  if (kIsWeb) usePathUrlStrategy();
   runApp(
     ProviderScope(
       observers: [RiverpodObserver()],
