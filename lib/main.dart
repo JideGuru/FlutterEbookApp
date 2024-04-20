41185cde77486d3c4f95af06912ceed497e8c8db
jideguru <guruliciousjide@gmail.com>
2024-04-20 17:25:11 +0100

    feat: usePathUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ebook_app/src/app.dart';
 import 'package:flutter_ebook_app/src/common/common.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:sembast/sembast.dart';
 
 void main() async {
